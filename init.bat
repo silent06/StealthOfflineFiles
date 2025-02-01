f95ab9193c302428e95bbcac935b652d36e321e8
silent06 <44829491+silent06@users.noreply.github.com>
2025-02-01 01:48:55 -0800

    Updated

--- init.bat
+++
@@ -1,7 +0,0 @@
-git init -b main
-git remote add upstream https://github.com/silent06/StealthOfflineFiles.git
-git pull upstream main
-git add .
-git commit -m "Committed Files"
-git push upstream main
-pause
